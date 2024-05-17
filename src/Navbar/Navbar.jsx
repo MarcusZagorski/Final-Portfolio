a51cff850f95cb96faf691d7a99a76be16288f2b
Marcus <marcus.ats94@gmail.com>
2024-05-17 23:04:25 +0100

    Refactored Navbar.jsx

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -10,96 +10,45 @@ const Navbar = ({
   scrollToHome,
   scrollToProjects,
 }) => {
-  const homeScrollTarget = () =>
-    scrollToHome.current.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-
-  const aboutScrollTarget = () =>
-    scrollToAbout.current.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-
-  const projectsScrollTarget = () =>
-    scrollToProjects.current.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
+  const nav = (navTitle, animateInView, scrollTo) => {
+    return (
+      <motion.li
+        onClick={() =>
+          scrollTo.current.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+          })
+        }
+        animate={
+          animateInView
+            ? {
+                color: "rgb(102 102 102)",
+                backgroundColor: "white",
+                borderRadius: "8px",
+                padding: "0.1rem 0.2rem",
+                width: "fit-content",
+                marginLeft: "auto",
+                rotate: "2deg",
+                scale: 1.1,
+              }
+            : {
+                padding: "0",
+                width: "fit-content",
+                marginLeft: "auto",
+              }
+        }
+      >
+        {navTitle}
+      </motion.li>
+    );
+  };
 
   return (
     <nav>
       <ul className="nav">
-        <motion.li
-          onClick={homeScrollTarget}
-          animate={
-            homeInView && !aboutInView
-              ? {
-                  color: "rgb(102 102 102)",
-                  backgroundColor: "white",
-                  borderRadius: "8px",
-                  padding: "0.1rem 0.2rem",
-                  width: "fit-content",
-                  marginLeft: "auto",
-                  rotate: "2deg",
-                  scale: 1.1,
-                }
-              : {
-                  padding: "0",
-                  width: "fit-content",
-                  marginLeft: "auto",
-                }
-          }
-        >
-          Home
-        </motion.li>
-        <motion.li
-          animate={
-            aboutInView
-              ? {
-                  color: "rgb(102 102 102)",
-                  backgroundColor: "white",
-                  borderRadius: "8px",
-                  padding: "0.1rem 0.2rem",
-                  width: "fit-content",
-                  marginLeft: "auto",
-                  rotate: "2deg",
-                  scale: 1.1,
-                }
-              : {
-                  padding: "0",
-                  width: "fit-content",
-                  marginLeft: "auto",
-                }
-          }
-          onClick={aboutScrollTarget}
-        >
-          About
-        </motion.li>
-        <motion.li
-          animate={
-            projectInView && !aboutInView
-              ? {
-                  color: "rgb(102 102 102)",
-                  backgroundColor: "white",
-                  borderRadius: "8px",
-                  padding: "0.1rem 0.2rem",
-                  width: "fit-content",
-                  marginLeft: "auto",
-                  rotate: "2deg",
-                  scale: 1.1,
-                }
-              : {
-                  padding: "0",
-                  width: "fit-content",
-                  marginLeft: "auto",
-                }
-          }
-          onClick={projectsScrollTarget}
-        >
-          Projects
-        </motion.li>
+        {nav("Home", homeInView && !aboutInView, scrollToHome)}
+        {nav("About", aboutInView, scrollToAbout)}
+        {nav("Projects", projectInView && !aboutInView, scrollToProjects)}
         <li>Download CV</li>
       </ul>
     </nav>
